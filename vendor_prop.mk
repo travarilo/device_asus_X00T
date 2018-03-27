1620bcd9377882d8e77af49c43efef0cdb6a7623
Jorim Jaggi <jjaggi@google.com>
2018-03-27 15:57:47 +0200

    X00TD: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank
    Bug: 73640835
    Change-Id: I6ea9baa28acc222afcf365f3bf107149bc8974d9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=196610 \
 	ro.sf.lcd_density=420 \
 	vendor.display.enable_default_color_mode=1
+	sdm.debug.rotator_downscale=1
 
 # LED
 PRODUCT_PROPERTY_OVERRIDES += \
