0eceb33801e36450b0b260597eaa95e3c30318dd
Vinay Verma <vvinay@codeaurora.org>
2018-04-16 07:56:07 +0530

    X00T: Add MATRIX file for vendor HAL's
    Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,6 +206,7 @@ PRODUCT_SOONG_NAMESPACES += \
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
