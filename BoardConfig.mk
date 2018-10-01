c9001a6e956efe78d475e4500f5645f931954557
Rizky Benggolo <travarilo@gmail.com>
2018-10-01 02:55:50 +0000

    X00TD: include qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -259,7 +259,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 VENDOR_SECURITY_PATCH := 2018-08-01
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Use Snapdragon LLVM, if available
