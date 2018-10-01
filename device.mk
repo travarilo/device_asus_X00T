12e55a120ee9762eb5d4adb24aeaf56a6d8e49d8
Rizky Benggolo <travarilo@gmail.com>
2018-10-01 11:47:10 +0700

    X00TD: Add prebuilt Google Camera
    GCam-5.1.018-Pixel2Mod-Arnova8G2-V8.3b1.apk

--- device.mk
+++ device.mk
@@ -130,7 +130,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
     libxml2 \
-    Snap
+    GoogleCamera
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
