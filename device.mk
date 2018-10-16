b57867360d200324bcb92ec9f7bb0fbb859f0825
Davide Garberi <dade.garberi@gmail.com>
2018-10-16 19:45:32 +0200

    X00TD: Update audio and soundtrigger services
    * The blobs are from LA.UM.7.2.r1-04000-sdm660.0 - B2N_sprout:9/PPR1.180610.011/00WW_3_22C
    
    Change-Id: I4c2ae0d6cca87c090020d43ae06a4f13b927130e
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.0-service \
     audio.a2dp.default \
     audio_amplifier.sdm660 \
     audio.primary.sdm660 \
