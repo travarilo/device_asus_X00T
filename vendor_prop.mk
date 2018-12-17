254a59fd98a5bb3cb9333caf9ccdbe6f13b5032d
Sundong Ahn <sundongahn@google.com>
2018-12-17 16:40:39 +0900

    X00T: Add system properties for surfaceflinger
    The configstore will be not used in surfaceflinger. So surfaceflinger
    will use the system properties instead of configstore.
    For this, the system properties are added.
    
    Bug: 112386364
    Test: m -j && boot test
    Change-Id: Ie3d2adccca06702217984cf2c9db986d852b7014

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -274,7 +274,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 # System prop for UBWC
 PRODUCT_PROPERTY_OVERRIDES += \
