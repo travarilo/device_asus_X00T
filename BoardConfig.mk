62c1492772d98fab3603f5d0e49d204a35356204
Albert I <krascgq@outlook.co.id>
2019-01-25 03:38:04 +0800

    X00T: Update board configuration for 15.2016.1901.339
    Change-Id: I27ac73b4c7de906ba28cb1cf957a4a5f7a602d87
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -263,7 +263,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-11-01
+VENDOR_SECURITY_PATCH := 2019-01-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
