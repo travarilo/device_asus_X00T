630402ab1b8f5273a81ef0e290e06d2df8045695
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-04-19 16:42:58 -0300

    X00T: Ignore neverallows
    Change-Id: Ibb8512f476b6997a6bebc2901f0f017de70c500b
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -253,6 +253,7 @@ VENDOR_SECURITY_PATCH := 2019-05-01
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_X00T
