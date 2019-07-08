54a05aba6ec9b8ffd908ab3bafd65cec3f975ade
Atman <masteratman@gmail.com>
2019-07-08 04:30:28 +0000

    X00T: Enable debug.sf.latch_unsignaled
    * Reduces Jank
    
    Signed-off-by: Atman <masteratman@gmail.com>
    Change-Id: I1f413a6f68359112b017423f939e191c46506117

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -107,6 +107,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.latch_unsignaled=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
     debug.cpurend.vsync=false \
