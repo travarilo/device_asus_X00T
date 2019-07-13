1d0e5a574eac2963fa9ed5972a26ad745c05d8fa
Rizky Benggolo <travarilo@gmail.com>
2019-07-13 08:50:30 +0000

    Revert "X00T: set HWUI_COMPILE_FOR_PERF"
    This reverts commit 4d4943cb1459d6588f7b2302053b755341447d86.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,9 +213,6 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 
-# HWUI
-HWUI_COMPILE_FOR_PERF := true
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
