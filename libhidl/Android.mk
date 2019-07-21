4a4d3152a903652c16a86b07bdc86ab9fd766e37
Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>
2019-07-21 22:16:15 +0530

    commented lines for hidl.base@1.0 module already present in pixledust source
    Signed-off-by: Harshwardhan Mehrotra <harshwardhanmehrotra@gmail.com>

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -13,13 +13,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
-LOCAL_MODULE := android.hidl.base@1.0
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
+#include $(CLEAR_VARS)
+#LOCAL_SHARED_LIBRARIES := libhidltransport
+#LOCAL_MODULE := android.hidl.base@1.0
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+#LOCAL_VENDOR_MODULE := true
+#include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := libhidltransport
