repo-x00td/android_device_asus_X00T

X00T: HACK: display correct baseband version

rootdir/Android.mk
@@ -75,6 +75,14 @@ LOCAL_SRC_FILES    := bin/move_wifi_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fix_baseband.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/fix_baseband.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # etc
 
 include $(CLEAR_VARS)

travarilo/device_asus_X00T

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

rootdir/Android.mk
@@ -91,6 +91,14 @@ LOCAL_SRC_FILES    := bin/init.qcom.ipastart.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := fix_baseband.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/fix_baseband.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
 # etc
 
 include $(CLEAR_VARS)