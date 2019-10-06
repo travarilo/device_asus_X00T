9290e0f52b7ec83913e97585675a0746a2c9e5e0
Rizky Benggolo <travarilo@gmail.com>
2019-10-06 22:56:43 +0000

    X00T: [HACK]: Use walleye oreo fingerprint to pass safetynet

--- havoc_X00T.mk
+++ havoc_X00T.mk
@@ -37,10 +37,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1285 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.021 4565141 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1910.059-20190920:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys
 
 HAVOC_BUILD_TYPE := Official
 PRODUCT_PROPERTY_OVERRIDES += \
