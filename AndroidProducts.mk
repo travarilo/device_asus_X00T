pranavkapoor001/Device_Asus_X00T_Old

X00T: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+      lineage_X00T-userdebug

travarilo/device_asus_X00T

AndroidProducts.mk
@@ -16,3 +16,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_X00T-userdebug
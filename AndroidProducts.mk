a8462efaa3c70f816f8892ee601e9de808d71042
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    X00T: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,3 +16,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/havoc_X00T.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_X00T-userdebug
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-add_lunch_combo havoc_X00T-userdebug
-add_lunch_combo havoc_X00T-user
-add_lunch_combo havoc_X00T-eng
