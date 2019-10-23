84efb983a5c5899e6c961a7c4f88b19347800518
Rizky Benggolo <travarilo@gmail.com>
2019-10-23 11:31:05 +0700

    X00T: Sync with stock rom 059

--- havoc_X00T.mk
+++ havoc_X00T.mk
@@ -37,10 +37,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1181 release-keys"
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1 1285 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1905.053-20190513:user/release-keys
+BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.1910.059-20190920:user/release-keys
 
 HAVOC_BUILD_TYPE := Official
 PRODUCT_PROPERTY_OVERRIDES += \
