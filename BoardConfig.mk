6629e0f96f9f4d4ef554116b79fee0aca7d3d248
Anushek Prasal <anushekprasal@gmail.com>
2019-11-11 12:59:36 +0530

    X00T: Set TARGET_USES_NON_LEGACY_POWERHAL to true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,6 +231,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/kernel/touchpanel/dclicknode"
+TARGET_USES_NON_LEGACY_POWERHAL := true
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
