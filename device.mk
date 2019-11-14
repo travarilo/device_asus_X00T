8f584c4734eb1a8b7c3233d879793f949a0dc856
Rizky Benggolo <travarilo@gmail.com>
2019-11-14 11:39:14 +0000

    X00T: Disable FM
    Change-Id: I8fbc7176ec00306046becdc5454928b0b4159e46

--- device.mk
+++ device.mk
@@ -219,15 +219,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
-
 # GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
