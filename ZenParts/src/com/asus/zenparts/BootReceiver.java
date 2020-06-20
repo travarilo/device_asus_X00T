42576cc08f97c8eed9c328975ad7418795bfca30
SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
2020-06-20 15:15:37 +0530

    X00T: ZenParts: Add SELinux Switch
    Reference: https://github.com/DerpLab/platform_packages_apps_DerpQuest/commit/65f48a63a8e43ca041462a9c2280435ad15779a6
    
    Vectors From: https://materialdesignicons.com
    
    @SiddharthBharadwaj edits: Adapt To Parts
    
    Change-Id: Iac55a9065152e747fa2890f4dd8341c7ddf03048

--- ZenParts/src/com/asus/zenparts/BootReceiver.java
+++ ZenParts/src/com/asus/zenparts/BootReceiver.java
@@ -16,13 +16,22 @@
 
 package com.asus.zenparts;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import com.asus.zenparts.preferences.VibratorStrengthPreference;
+import android.content.SharedPreferences;
+import android.os.SELinux;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.asus.zenparts.R;
 import com.asus.zenparts.kcal.Utils;
+
+import java.io.IOException;
+import java.util.List;
 
 public class BootReceiver extends BroadcastReceiver implements Utils {
 
@@ -35,7 +44,66 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
     private final String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
     private final String MICROPHONE_GAIN_PATH = "/sys/kernel/sound_control/mic_gain";
 
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+
+    private Context settingsContext = null;
+    private static final String TAG = "SettingsOnBoot";
+    private boolean mSetupRunning = false;
+    private Context mContext;
+
     public void onReceive(Context context, Intent intent) {
+
+    mContext = context;
+    ActivityManager activityManager =
+            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+    List<ActivityManager.RunningAppProcessInfo> procInfos =
+            activityManager.getRunningAppProcesses();
+    for(int i = 0; i < procInfos.size(); i++) {
+        if(procInfos.get(i).processName.equals("com.google.android.setupwizard")) {
+            mSetupRunning = true;
+        }
+    }
+
+    if(!mSetupRunning) {
+        try {
+            settingsContext = context.createPackageContext("com.android.settings", 0);
+        } catch (Exception e) {
+            Log.e(TAG, "Package not found", e);
+        }
+        SharedPreferences sharedpreferences = context.getSharedPreferences("selinux_pref",
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(PREF_SELINUX_MODE)) {
+            boolean currentIsSelinuxEnforcing = SELinux.isSELinuxEnforced();
+            boolean isSelinuxEnforcing =
+                    sharedpreferences.getBoolean(PREF_SELINUX_MODE,
+                            currentIsSelinuxEnforcing);
+            if (isSelinuxEnforcing) {
+                if (!currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 1");
+                        showToast(context.getString(R.string.selinux_enforcing_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                if (currentIsSelinuxEnforcing) {
+                    try {
+                        SuShell.runWithSuCheck("setenforce 0");
+                        showToast(context.getString(R.string.selinux_permissive_toast_title),
+                                context);
+                    } catch (SuShell.SuDeniedException e) {
+                        showToast(context.getString(R.string.cannot_get_su), context);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
         FileUtils.setValue(TORCH_1_BRIGHTNESS_PATH,
                 Settings.Secure.getInt(context.getContentResolver(),
@@ -76,6 +144,13 @@ public class BootReceiver extends BroadcastReceiver implements Utils {
             FileUtils.setValue(KCAL_HUE, Settings.Secure.getInt(context.getContentResolver(),
                     PREF_HUE, HUE_DEFAULT));
         VibratorStrengthPreference.restore(context);
+
+
         }
     }
+    private void showToast(String toastString, Context context) {
+    Toast.makeText(context, toastString, Toast.LENGTH_SHORT)
+            .show();
 }
+}
+
