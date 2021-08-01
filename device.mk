7b5e736fb60af8670b99ddf15b4ee7a048a3acb3
Rizky Benggolo <travarilo@gmail.com>
2021-08-01 22:26:56 +0700

    X00T: Build livedisplay
    Change-Id: I5c786765bba5eaa3370f7ec1f52f8d3284c64f9a

--- device.mk
+++ device.mk
@@ -291,6 +291,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.asus_X00T
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
